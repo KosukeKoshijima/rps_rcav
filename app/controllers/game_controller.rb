a836512a6094f38c7a318a93a8a005dbe1d66b47
Kosuke Koshijima <kosuke.koshijima@gmail.com>
2017-04-21 10:04:53 -0500

    4/21-1

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,25 @@
+class GameController < ApplicationController
+  def play_rock
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+
+    render("game/play_rock.html.erb")
+  end
+
+  def play_paper
+    moves = ["rock", "paper", "scissors"]
+    @computer_move = moves.sample
+
+
+    if @computer_move == "rock"
+    @outcome = "win"
+    elsif @computer_move == "paper"
+    @outcome = "Tie"
+    else @computer_move == "scissors"
+    @outcome = "lose"
+    end
+
+    render("game/play_paper.html.erb")
+
+  end
+end
